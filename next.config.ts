6542fd02477187d19272f6bc69b909ecf1eb8ba3
Aayush Shrestha <be2020s698@gces.edu.np>
2026-07-28 16:31:10 +0545

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "appapi.rebuzzpos.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   experimental: {
